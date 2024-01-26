2a291f24bd13783e9196067c0ad1b7185c9fd961
jayad <jp@gmail.com>
2024-01-26 14:05:02 +0530

    Initial commit

---
+++ Day1_Code_Structure.cpp
@@ -0,0 +1,12 @@
+// inpute output stream
+#include <iostream>
+// use standard namespace
+using namespace std;
+// main function
+int main()
+{
+    // to print : cout <<
+    cout << "Hello World";
+    // return 0
+    return 0;
+}
